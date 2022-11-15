83849ccb049d6877147acf44f25fd34d1930eebb
SrajeshTuladhar <srajesh.tuladhar@deerwalk.edu.np>
2022-11-15 09:57:54 +0545

    Module Links Added

---
+++ module.js
@@ -0,0 +1,11 @@
+// exporting the variable
+export const name = 'JavaScript Program';
+
+// exporting the function
+export function sum(x, y) {
+    return x + y;
+}
+
+//https://www.programiz.com/javascript/modules 
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
